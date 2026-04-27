be6607ec002b81b29e94d89afb408eb8fe5334d2
Gereon <gereon.risse@gmail.com>
2026-04-27 14:23:30 +0200

    adjusted default frequencies

--- src/lib/state/options.svelte.ts
+++ src/lib/state/options.svelte.ts
@@ -1,6 +1,6 @@
 // State for analysis options
 
-const DEFAULT_FREQUENCIES = [80, 200, 800, 2000, 8000];
+const DEFAULT_FREQUENCIES = [200, 2000];
 
 export const options = $state({
 	// Crossover frequencies in Hz; bands are the intervals between them
